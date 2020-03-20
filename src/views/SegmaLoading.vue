14fc3f5bb424b78a684528fae1a7431b9daaafc1
LiDan.Chen@cisdi.com.cn <LiDan.Chen@cisdi.com.cn>
2020-03-20 10:57:15 +0800

    feat(loading):loading展示案例

--- src/views/SegmaLoading.vue
+++ src/views/SegmaLoading.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="segma-loading"
-         segma-loading-text="拼命加载中"
-         segma-loading-spinner="el-icon-loading"
-         segma-loading-background="rgba(0, 0, 0, 0.2)"
-         v-segma-loading="true"></div>
+    <div>
+        <div class="segma-loading"
+             segma-loading-text="拼命加载中"
+             segma-loading-spinner="el-icon-loading"
+             segma-loading-background="rgba(0, 0, 0, 0.2)"
+             v-segma-loading="loading"></div>
+        <div @click="loading=!loading">loading</div>
+    </div>
+
 </template>
 
 <script>
 export default {
-    name: "SegmaLoading"
+    name: "SegmaLoading",
+    data() {
+        return {
+            loading: false
+        }
+    },
 }
 </script>
 
