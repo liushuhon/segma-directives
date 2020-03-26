d85926f47c0b23ebbb18975db56af9b4b7655b4b
liuliu <815835778@qq.com>
2020-03-26 15:46:56 +0800

    feat(项目)：测试环境配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+
+let plugin = [];
+if(process.env["ENV"] === "test") {
+    plugin.push('istanbul');
+
 }
+module.exports = {
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+
+    plugins: ['istanbul']
+};
